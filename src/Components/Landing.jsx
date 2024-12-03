e3c31bc4f49e6d9f407229cb2db6fedeac710217
ANAMIKA V <Anamika-pixel>
2024-12-03 12:22:51 +0530

    Book-Search

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Landing() {
   return (
@@ -11,8 +12,10 @@ function Landing() {
           <Col>
             <h1 style={{ fontSize: '3rem', color: '#343a40', fontWeight: 'bold' }}>Search Books</h1>
             <p style={{ fontSize: '1.25rem', color: '#6c757d' }}>Discover your next great read from our collection.</p>
-            <Button variant="primary" size="lg" href="/home" style={{ fontSize: '1.25rem', padding: '12px 24px' }}>Start Searching</Button>
-          </Col>
+<Link to={'/home'}>
+              <Button variant="primary" size="lg"  style={{ fontSize: '1.25rem', padding: '12px 24px' }}>Start Searching</Button>
+  
+</Link>          </Col>
         </Row>
       </Container>
     </div>
